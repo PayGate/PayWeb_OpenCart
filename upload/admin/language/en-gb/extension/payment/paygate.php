1b9b5f306becc508e49adaa72b9d15bed5cc8dc0
Bruce Atkinson <bruce@appinlet.com>
2020-11-05 02:43:12 +0200

    2020-11-04: v3.0.4
    - Combine iFrame and Redirect implementations.
    - Add 'Disable IPN' feature.
    - Add' Test Mode' feature.
    - Remove session in redirect callback for improved reliability.
    - Add 'Payment Types' feature.

--- upload/admin/language/en-gb/extension/payment/paygate.php
+++ upload/admin/language/en-gb/extension/payment/paygate.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Copyright (c) 2019 PayGate (Pty) Ltd
+ * Copyright (c) 2020 PayGate (Pty) Ltd
  *
  * Author: App Inlet (Pty) Ltd
- * 
+ *
  * Released under the GNU General Public License
  */
 
@@ -15,23 +15,41 @@ $_['text_payment']   = 'Payment';
 $_['text_success']   = 'You have successfully configured the PayGate payment module.';
 $_['text_edit']      = 'Edit PayGate';
 $_['text_extension'] = 'Extensions';
+$_['text_notify']    = 'Disabled';
+$_['text_redirect']  = 'Enabled';
+$_['text_iframe']    = 'iFrame';
+$_['text_noframe']   = 'Redirect';
+$_['text_testmode']  = 'Enabled';
+$_['text_livemode']  = 'Disabled';
+$_['text_methodyes'] = 'Enable Payment Type';
+$_['text_methodno']  = 'Disable Payment Type';
 
 // Entry
-$_['entry_total']            = 'Total';
-$_['entry_order_status']     = 'Initial Order Status';
-$_['entry_geo_zone']         = 'Geo Zone';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Sort Order';
-$_['text_paygate']           = '<a onclick="window.open(\'https://www.paygate.co.za/\');"><img src="view/image/payment/paygate.gif" alt="PayGate" title="PayGate" style="border: 1px solid #EEEEEE;" /></a> PayWeb v3';
-$_['entry_merchant_id']      = 'PayGate ID:';
-$_['entry_merchant_key']     = 'PayGate Encryption Key:';
-$_['entry_success_status']   = 'Successful Order status:';
-$_['entry_failed_status']    = 'Failed Order Status:';
-$_['entry_cancelled_status'] = 'Cancelled Order Status:';
+$_['entry_total']              = 'Total';
+$_['entry_order_status']       = 'Initial';
+$_['entry_geo_zone']           = 'Geo Zone';
+$_['entry_status']             = 'Status';
+$_['entry_sort_order']         = 'Sort Order';
+$_['text_paygate']             = '<a onclick="window.open(\'https://www.paygate.co.za/\');"><img src="view/image/payment/paygate.png" alt="PayGate" title="PayGate" style="border: 1px solid #EEEEEE;" /></a>';
+$_['entry_merchant_id']        = 'PayGate ID';
+$_['entry_merchant_key']       = 'Encryption Key';
+$_['entry_success_status']     = 'Successful';
+$_['entry_failed_status']      = 'Failed';
+$_['entry_cancelled_status']   = 'Cancelled';
+$_['entry_notify_redirect']    = 'Disable IPN';
+$_['entry_iframe']             = 'Implementation';
+$_['entry_testmode']           = 'Test Mode';
+$_['entry_creditcardmethod']   = 'Enable Card on Checkout';
+$_['entry_banktransfermethod'] = 'Enable SiD Secure EFT on Checkout';
+$_['entry_zappermethod']       = 'Enable Zapper on Checkout';
+$_['entry_mobicredmethod']     = 'Enable Mobicred on Checkout';
+$_['entry_momopaymethod']      = 'Enable MoMoPay on Checkout';
+$_['entry_masterpassmethod']   = 'Enable MasterPass on Checkout';
 
 // Tab
 $_['tab_general']      = 'General';
-$_['tab_order_status'] = 'Order status';
+$_['tab_order_status'] = 'Order Status';
+$_['tab_pay_methods']  = 'Payment Types';
 
 // Help
 $_['help_total'] = 'The checkout total the order must reach before this payment method becomes active.';
