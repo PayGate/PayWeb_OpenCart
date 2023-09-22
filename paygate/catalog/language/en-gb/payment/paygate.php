9d94e04f2d4e9989f918d4ff96cfe3a748904ffe
Bruce Atkinson <bruce@appinlet.com>
2023-09-22 22:29:26 +0200

    2023-09-22: v3.1.0
    - Update for PHP 8.0.
    - Test on OpenCart 4.0.2.2.

---
+++ paygate/catalog/language/en-gb/payment/paygate.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright (c) 2023 Payfast (Pty) Ltd
+ *
+ * Author: App Inlet (Pty) Ltd
+ *
+ * Released under the GNU General Public License
+ */
+
+// Text
+$_['text_title']            = 'Paygate';
+$_['text_paygate_checkout'] = 'Pay via Paygate';
